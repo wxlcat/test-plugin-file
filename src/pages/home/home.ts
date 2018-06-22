22049a3c567a9a8593b3abe6b3e1076ed632e9fd
wxl <wxlcat@gmail.com>
2018-06-22 14:32:06 +0800

    File测试

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ImagePicker, ImagePickerOptions } from '@ionic-native/image-picker';
+import { File, FileEntry, IFile } from '@ionic-native/file';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,34 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private imagePicker: ImagePicker, private file: File) {
 
+  }
+
+  onChoose() {
+    this.chooseFromAlbum(1, 400, 400).then(
+      path => {
+        console.warn('chooseFromAlbum ImgPath: ' + path);
+
+        this.file.readAsArrayBuffer(path, name)
+        .then(arrayBuf => {
+          console.warn('readAsArrayBuffer: ' + arrayBuf);
+        }).catch(error => {
+          console.log("readAsArrayBuffer catch:", error);
+        });
+      })
+      .catch(reason=>{
+        console.warn('chooseFromAlbum catch: ' + reason);
+      });
+  }
+
+  private chooseFromAlbum(count: number = 1, width: number = 0, height: number = 0): Promise<any> {
+    const options: ImagePickerOptions = {
+      maximumImagesCount: count,
+      width: width,
+      height: height
+    };
+    return this.imagePicker.getPictures(options);
   }
 
 }
